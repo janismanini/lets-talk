6e753b595e4f3a07c4c1a78cb63e1ea47ca55e43
jkanga <jkanga@3pc.de>
2022-12-15 13:10:18 +0100

    add interview page

---
+++ src/views/InterviewPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="interview">
+    <h1>{{ interview.name }}</h1>
+    <h2>{{ interview.location }}</h2>
+    <img
+      :src="`../src/assets/images/${interview.portrait}`"
+      :alt="interview.name"
+    />
+    <h3>{{ interview.intro }}</h3>
+    <ul
+      v-for="question in interview.questions"
+      :key="interview.questions.indexOf(question)"
+    >
+      <li>
+        <h4>{{ question.question }}</h4>
+        <p>{{ question.answer }}</p>
+      </li>
+    </ul>
+    <p>{{ interview.thanks }}</p>
+    <p>{{ interview.imprint }}</p>
+  </section>
+</template>
+
+<script>
+import sourceData from "../data.json";
+
+export default {
+  computed: {
+    interviewSlug() {
+      return this.$route.params.slug;
+    },
+    interview() {
+      return sourceData.interviews.find((i) => i.slug === this.interviewSlug);
+    },
+  },
+};
+</script>
