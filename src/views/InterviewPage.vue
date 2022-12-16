32a6ce72e31cd00dec24177f35d956b4a6a39a6e
jkanga <jkanga@3pc.de>
2022-12-16 12:50:16 +0100

    add images to interview pages

--- src/views/InterviewPage.vue
+++ src/views/InterviewPage.vue
@@ -4,16 +4,24 @@
     <h2>{{ interview.location }}</h2>
     <img
       :src="`../src/assets/images/${interview.portrait}`"
-      :alt="interview.name"
+      :alt="`interview with ${interview.name}`"
     />
     <h3>{{ interview.intro }}</h3>
-    <ul
-      v-for="question in interview.questions"
-      :key="interview.questions.indexOf(question)"
-    >
-      <li>
-        <h4>{{ question.question }}</h4>
+    <ul>
+      <li
+        v-for="question in interview.questions"
+        :key="interview.questions.indexOf(question)"
+      >
+        <h4>{{ question.title }}</h4>
         <p>{{ question.answer }}</p>
+        <div class="interview__images" v-if="question.images.length">
+          <img
+            v-for="image in question.images"
+            :key="question.images.indexOf(image)"
+            :src="`../src/assets/images/${image}`"
+            :alt="`interview with ${interview.name}`"
+          />
+        </div>
       </li>
     </ul>
     <p>{{ interview.thanks }}</p>
