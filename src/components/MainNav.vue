9b62fec89cec8873fddbb748fd965500980f4dd6
jkanga <jkanga@3pc.de>
2022-12-25 11:37:22 +0100

    add styles

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,12 +1,17 @@
 <template>
   <nav class="navigation">
     <ul class="navigation__list">
-      <li v-for="interview in interviews" :key="interview.id">
-        <interview-link :interview="interview">
-          {{ interview.name }}
-        </interview-link>
+      <li class="has-submenu">
+        <router-link to="/" aria-expanded="false">interviews</router-link>
+        <ul class="navigation__list-interviews">
+          <li v-for="interview in interviews" :key="interview.id">
+            <interview-link :interview="interview">
+              {{ interview.name }}
+            </interview-link>
+          </li>
+        </ul>
       </li>
-      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/about">about</router-link></li>
     </ul>
   </nav>
 </template>
@@ -28,6 +33,22 @@ export default {
 </script>
 
 <style>
+.navigation {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 40%;
+}
+
+.navigation__list {
+  display: flex;
+  justify-content: space-around;
+}
+
+.navigation__list-interviews {
+  display: none;
+}
+
 nav .router-link-active {
   border-bottom: 2px solid;
 }
